90497761bf62a96ac8835944458c8b8cd55a70ac
Marko Ristin <marko@ristin.ch>
2023-10-05 08:58:29 +0200

    Reformat with WebStorm (#27)
    We manually reformat the code with WebStorm for easier diffing.

--- game.js
+++ game.js
@@ -5,18 +5,29 @@ function determineAndLogBrowser() {
     // NOTE (mristin):
     // See https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browsers
 
-    const browser = (function() {
-        var test = function(regexp) {return regexp.test(window.navigator.userAgent)}
+    const browser = (function () {
+        var test = function (regexp) {
+            return regexp.test(window.navigator.userAgent)
+        }
         switch (true) {
-            case test(/edg/i): return "Microsoft Edge";
-            case test(/trident/i): return "Microsoft Internet Explorer";
-            case test(/firefox|fxios/i): return "Mozilla Firefox";
-            case test(/opr\//i): return "Opera";
-            case test(/ucbrowser/i): return "UC Browser";
-            case test(/samsungbrowser/i): return "Samsung Browser";
-            case test(/chrome|chromium|crios/i): return "Google Chrome";
-            case test(/safari/i): return "Apple Safari";
-            default: return "Other";
+            case test(/edg/i):
+                return "Microsoft Edge";
+            case test(/trident/i):
+                return "Microsoft Internet Explorer";
+            case test(/firefox|fxios/i):
+                return "Mozilla Firefox";
+            case test(/opr\//i):
+                return "Opera";
+            case test(/ucbrowser/i):
+                return "UC Browser";
+            case test(/samsungbrowser/i):
+                return "Samsung Browser";
+            case test(/chrome|chromium|crios/i):
+                return "Google Chrome";
+            case test(/safari/i):
+                return "Apple Safari";
+            default:
+                return "Other";
         }
     })();
 
